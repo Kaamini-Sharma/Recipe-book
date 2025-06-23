efcd4127ea36c457596f1d07b155e7cefeeb02aa
Kaamini-Sharma <Kaamini.sh2609@gmail.com>
2025-06-24 02:05:51 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Make sure to import 'path'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Recipe-book/", // 👈 REQUIRED for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      // Correctly maps '@' to the absolute path of your 'src' directory
       "@": path.resolve(__dirname, "./src"),
     },
   },
