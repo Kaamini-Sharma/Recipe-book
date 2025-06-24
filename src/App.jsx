bc42a4b5050222ff0fc3971fa458864de169c0c2
Kaamini-Sharma <Kaamini.sh2609@gmail.com>
2025-06-24 11:17:45 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "@/pages/Home";
-import Addrecipe from "@/pages/Addrecipe";
-import recipeDetail from "@/pages/recipeDetail";
-import Editrecipe from "@/pages/Editrecipe";
+import AddRecipe from "@/pages/AddRecipe";
+import RecipeDetail from "@/pages/RecipeDetail";
+import EditRecipe from "@/pages/EditRecipe";
 import { Soup } from "lucide-react";
 // import { Link } from "react-router-dom";
 
@@ -17,23 +17,23 @@ function App() {
           className="flex items-center gap-2 text-2xl font-bold hover:text-orange-600 transition"
         >
           <Soup className="w-6 h-6 text-orange-500" />
-          recipe Book
+          Recipe Book
         </Link>
         <nav className="space-x-4">
           <Link to="/" className="text-blue-500 hover:underline">
             Home
           </Link>
           <Link to="/add" className="text-blue-500 hover:underline">
-            Add recipe
+            Add Recipe
           </Link>
         </nav>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add" element={<Addrecipe />} />
-        <Route path="/recipe/:id" element={<recipeDetail />} />
-        <Route path="/edit/:id" element={<Editrecipe />} />
+        <Route path="/add" element={<AddRecipe />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/edit/:id" element={<EditRecipe />} />
         <Route path="*" element={<div className="p-8">Page Not Found </div>} />
       </Routes>
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
-import recipeCard from "@/components/ui/recipeCard";
+import RecipeCard from "@/components/ui/RecipeCard";
 import { motion } from "framer-motion";
 
 const Home = () => {
-  const [recipes, setrecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const stored = JSON.parse(localStorage.getItem("recipes")) || [];
-    setrecipes(stored);
+    setRecipes(stored);
   }, []);
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-orange-50 to-white px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-6">
-        🍽 recipe Sharing Book
+        🍽 Recipe Sharing Book
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe, i) => (
@@ -23,7 +23,7 @@ const Home = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4, delay: i * 0.1 }}
           >
-            <recipeCard recipe={recipe} />
+            <RecipeCard recipe={recipe} />
           </motion.div>
         ))}
       </div>
